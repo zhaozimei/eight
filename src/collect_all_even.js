7f7cdd9551edcdfafb9b9a46a56ccd1c1d622eb7
zhaozimei <1140173180@qq.com>
2016-05-29 21:59:33 +0800

    Update collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,7 +1,13 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+  var arr = [];
+  for(var i=0; i<collection.length; i++){
+	  if(collection[i]%2 == 0){
+		  arr.push(collection[i])
+	   }
+  }
+  return arr;
 }
 
 module.exports = collect_all_even;
